fd1d20104f92d762573768bd0c7d9817f59a9d64
David Stavis <davidstavis@gmail.com>
2016-09-23 23:38:01 +0800

    Made duplication prevention case-insensitive

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,6 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :papers
 
-  validates :name, presence: true, uniqueness: true
+  validates_presence_of :name
+  validates_uniqueness_of :name, case_sensitive: false
 end
