574b9a07946849f9a0a99f88229fc1995f02207a
David Stavis <davidstavis@gmail.com>
2016-12-09 17:13:15 +0800

    Added a validation to the Tag model preventing uppercase letters in tag names. Literally anything else is allowed. Hope that doesn't create an issue down the line ;)

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,7 @@ class Tag < ActiveRecord::Base
   has_and_belongs_to_many :papers
   validates_presence_of :name
   validates_uniqueness_of :name, case_sensitive: false
+
+  validates :name, :format => { with: /\A[^A-Z]+\z/,
+    message: "Only lowercase letters allowed" }
 end
